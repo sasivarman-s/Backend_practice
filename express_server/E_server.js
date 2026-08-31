463485751285a4cba661b115c9fe93382d3feb33
sasivarman-s <sureshraman2680@gmail.com>
2026-08-31 15:23:05 +0530

    created multiple endpoints

--- express_server/E_server.js
+++ express_server/E_server.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const app = express()
 app.get("/", (req, res) => {
-    res.send("this is express.js server")
+    res.send("sasivarman's server by express.js")
 })
-const PORT = 5000
+app.get("/home", (req, res) => {
+    res.send(" this sasivarman's home page")
+})
+app.get("/plan", (req, res) => {
+    res.send("sasivarman's weekly/monthly plans are availible!")
+})
+app.get("/admin", (req, res) => {
+    res.send("this is admin page \n enter you details:...")
+})
+const PORT = 2008
 app.listen(PORT, () => {
     console.log("Api is listening");
 })
